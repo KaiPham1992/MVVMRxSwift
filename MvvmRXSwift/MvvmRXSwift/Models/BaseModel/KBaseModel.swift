ca0276031f66b60fe3feded17a7ed133c92769ef
Kai <kai@vinova.sg>
2017-09-08 20:43:19 +0700

    add base API

---
+++ MvvmRXSwift/MvvmRXSwift/Models/BaseModel/KBaseModel.swift
@@ -0,0 +1,12 @@
+//
+//  KBaseModel.swift
+//  MvvmRXSwift
+//
+//  Created by Kai on 9/8/17.
+//  Copyright © 2017 Kai. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+protocol KBaseModel: BaseMappable {}
